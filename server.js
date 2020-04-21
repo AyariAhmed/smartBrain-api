f6a96a3cc2696459656a6b9628f873b6245cb1b5
Ayari Ahmed <ayari17@hotmail.fr>
2020-04-21 03:39:49 +0100

    setting db

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'ayari17',
-        database: 'smartbrain'
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
