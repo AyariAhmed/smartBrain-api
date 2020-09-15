e042e1e88789053df9eb969bb350455f402a5a68
Ayari17 <ayari17@hotmail.fr>
2020-09-15 15:30:20 +0100

    'FinalVersion'

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,7 +1,7 @@
 
 
 const handleSignin = (db, bcrypt) => (req, res)=> {
-    const { email, password } = req.body;
+    const { email , password } = req.body;
     if (!email ||  !password) {
         return res.status(400).json('Incorrect form submission!');
     }
